ba23271a67642d15b2fcd33ec34caf44530586a1
francesco-lapenna <francesco.lapenna.d@gmail.com>
2025-06-27 13:44:04 +0200

    Fix function declarations in PhyAuthP2P.h to include semicolons

--- OpenVLC_VL_IR/Driver/PhyAuthP2P.h
+++ OpenVLC_VL_IR/Driver/PhyAuthP2P.h
@@ -3,13 +3,13 @@
 
 static int hmac_sha256(const u8 *key, unsigned int keylen,
                        const u8 *data, unsigned int datalen,
-                       u8 *out, unsigned int outlen)
+                       u8 *out, unsigned int outlen);
 
-static u32 truncate(const u8 *hmac_result, size_t hmac_len)
+static u32 truncate(const u8 *hmac_result, size_t hmac_len);
 
-static int generate_potp(u8 *out)
+static int generate_potp(u8 *out);
 
-static void example_potp_usage(void)
+static void example_potp_usage(void);
 
 
 #endif
