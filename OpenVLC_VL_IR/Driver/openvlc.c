0677d9329a3e99e95d0e7616f48e324529f16d8e
francesco-lapenna <francesco.lapenna.d@gmail.com>
2025-06-27 11:34:37 +0200

    Update preamble handling in construct_frame_header and improve GET_PREAMBLE logic

--- OpenVLC_VL_IR/Driver/openvlc.c
+++ OpenVLC_VL_IR/Driver/openvlc.c
@@ -409,9 +409,9 @@ static void construct_frame_header(char* buffer, int buffer_len, int payload_len
     //unsigned short crc;
 
     for (i=0; i<PREAMBLE_LEN; i++)
-        buffer[i] = 0xf1; // Preamble
+        buffer[i] = 0xaa; // Preamble
     // SFD
-	buffer[1]= 0xae; // Added to synchronize correctly with the frame
+	//buffer[1]= 0xae; // Added to synchronize correctly with the frame
     buffer[PREAMBLE_LEN] = 0xa3; //10100011 0110011010100101
     // Length of payload
     buffer[PREAMBLE_LEN+1] = (unsigned char) ((payload_len>>8) & 0xff);
