7a0d1249577293a745caf7778a50f3ba036c5995
francesco-lapenna <francesco.lapenna.d@gmail.com>
2025-07-03 11:08:45 +0200

    fix

--- OpenVLC_VL_IR/Driver/openvlc.c
+++ OpenVLC_VL_IR/Driver/openvlc.c
@@ -755,8 +755,8 @@ static int phy_decoding(void *data)
 			
 			memcpy(&rx_data[2],&rx_pru[2],group_32bit*sizeof(unsigned int)); // 
 			
-			printk("\nReceived: ");
-			for (i=2; i<byte_len-20; i++) {
+			printk("Received: ");
+			for (i=2; i<byte_len; i++) {
 				/*int bit;
 				for (bit = 7; bit >= 0; bit--) {
 					printk("%d", (rx_data[i] >> bit) & 1);
